5a81d24311c13c5a5bcf5f3e036a891929f56a70
Jorge Perez <info@jperez.ca>
2024-07-26 16:55:46 +0700

    add toggle button to nav.tsx file, update deploy link & update footer links. nav.tsx & footer.tsx files updated

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,7 @@
-import Link from 'next/link'
+'use client';
+
+import Link from 'next/link';
+import { ModeToggle } from './mode-toggle';
 
 const navItems = {
   '/': {
@@ -7,7 +10,7 @@ const navItems = {
   '/blog': {
     name: 'blog',
   },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
+  'https://github.com/JPerez00/portfolio-starter-with-toggle': {
     name: 'deploy',
   },
 }
@@ -20,21 +23,26 @@ export function Navbar() {
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
+          <div className="flex flex-1">
+            <div className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 m-1">
+              <ModeToggle />
+            </div>
+          </div>
+          <div className="flex flex-row space-x-0">
             {Object.entries(navItems).map(([path, { name }]) => {
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 pl-2 m-1"
                 >
                   {name}
                 </Link>
-              )
+              );
             })}
           </div>
         </nav>
       </div>
     </aside>
-  )
+  );
 }
